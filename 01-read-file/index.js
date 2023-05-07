654eee7ec707a43924bc4a0f59ffd33ca76a8644
rebel228 <dinovik@rambler.ru>
2023-05-07 19:54:39 +0300

    feat: complete 01-read-file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const stream = fs.createReadStream('./01-read-file/text.txt', 'utf-8');
+let data = '';
+
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log('End', data));
