f98219fbe559ada204c632a349976a589211f380
rebel228 <dinovik@rambler.ru>
2023-05-07 23:12:36 +0300

    feat: complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
 
+const fs = require('fs');
+const path = require('path');
+
+function copyDir() {
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+    if (err) throw err;
+  });
+
+  fs.readdir(path.join(__dirname, 'files'), (error, files) => {
+    if(error) throw error;
+    const ogFiles = [];
+    files.forEach(file => {
+      ogFiles.push(file);
+      const src = path.join(__dirname, 'files', file);
+      const dest = path.join(__dirname, 'files-copy', file);
+      fs.copyFile(src, dest, error => {
+        if(error) throw error;
+      });
+    })
+
+    fs.readdir(path.join(__dirname, 'files-copy'), (error, files) => {
+      if(error) throw error;
+      files.forEach (file => {
+        if (!ogFiles.includes(file)) {
+          fs.unlink(path.join(__dirname, 'files-copy', file), error => {
+            if(error) throw error;
+          })
+        }
+      });
+    });
+  });
+};
+
+copyDir();
