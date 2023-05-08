aa22362c41b1b7c92a446bf49691b13a7537472f
rebel228 <dinovik@rambler.ru>
2023-05-08 15:58:18 +0300

    fix: remove rogue 'end' in cole.log in 1st task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,4 +3,4 @@ const stream = fs.createReadStream('./01-read-file/text.txt', 'utf-8');
 let data = '';
 
 stream.on('data', chunk => data += chunk);
-stream.on('end', () => console.log('End', data));
+stream.on('end', () => console.log(data));
